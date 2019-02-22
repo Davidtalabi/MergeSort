495d422389a646f28b5f8c5b48f202c47194f78b
David Talabi <Dtalabi@ucmerced.edu>
2019-02-21 20:48:42 -0800

    Update Mergesort.cpp

--- Mergesort.cpp
+++ Mergesort.cpp
@@ -88,7 +88,7 @@ cout << "Enter Array Elements!" << endl;
 for(int i=0; i<n; i++)
 	cin >> A[i]; //user inputs array elements
 
-	heapSort(A,n); //recursively calling heapsort function made earlier to sort the array
+	mergeort(A,n); //recursively calling heapsort function made earlier to sort the array
 	displayarray(A,n); //recursively calling display array function to display the final sorted array
 	
 return 0;
